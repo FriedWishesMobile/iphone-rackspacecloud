918cb63f70801494c99c939662778a5f31b105bf
Michael Mayo <mike@Michael-Mayos-MacBook-Pro.local>
2009-12-03 22:38:14 -0800

    beginning folder style navigation for cloud files

---
+++ Classes/ListFolderObjectsViewController.h
@@ -0,0 +1,16 @@
+//
+//  ListFolderObjectsViewController.h
+//  Rackspace
+//
+//  Created by Michael Mayo on 12/3/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface ListFolderObjectsViewController : UITableViewController {
+
+}
+
+@end
